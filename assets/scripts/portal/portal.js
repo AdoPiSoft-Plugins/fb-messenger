8aeb70dbea5061c27f9a44756ae850b1eea98d61
Arnel Lenteria <arnel.lenteria@gmail.com>
2021-06-02 21:02:32 +0800

    fix error - icon not showing

--- assets/scripts/portal/portal.js
+++ assets/scripts/portal/portal.js
@@ -24,26 +24,82 @@ function initFbChatButton(options, cb){
   };
 }
 
+function hideIcon(){
+  el = document.getElementById("wh-widget-send-button");
+  if (el) el.style.display = 'none';
+  var els = document.querySelectorAll('[id^=gb-widget]')
+  for(var i=0; i<els.length; i++){
+    var e = els[i];
+    e.style.display = 'none';
+  }
+}
+
+function showIcon(){
+  el = document.getElementById("wh-widget-send-button");
+  if (el) el.style.display = '';
+
+  var els = document.querySelectorAll('[id^=gb-widget]')
+  for(var i=0; i<els.length; i++){
+    var e = els[i];
+    e.style.display = '';
+  }
+}
+
+
 httpGet('/fb-chat-config', function(d){
   var options = JSON.parse(d)
   setTimeout(function(){
-    device_store.on('state', function(state){
-      if(typeof(WhWidgetSendButton)!='function')
-        initFbChatButton(options);
-      var is_connected = state.current.status == "connected";
-      if(options.hide_on_offline){
-        var el = document.getElementById("wh-widget-send-button");
-        if(is_connected)
-          el.style.display = '';
-        else
-          el.style.display = 'none';
+    if(typeof(device_store) == "undefined"){
+      var socket = Socket && Socket.getInstance()
+      device_store = {
+        events: {},
+        on: function(e, fn){
+          device_store.events[e] = device_store.events[e] || []
+          device_store.events[e].push(fn)
+        },
+        get: function(){
+          return {
+            status: socket.query.status
+          }
+        }
       }
+
+      socket._callbacks["$device:connected"].push(function(){
+        for(var i=0; i<= device_store.events["state"].length; i++){
+          var fn = device_store.events["state"][i]
+          if(typeof(fn) == 'function') fn({current: "connected"});
+        }
+      })
+
+      socket._callbacks["$device:disconnected"].push(function(){
+        for(var i=0; i<= device_store.events["state"].length; i++){
+          var fn = device_store.events["state"][i]
+          if(typeof(fn) == 'function') fn({current: "disconnected"});
+        }
+      })
+    }
+
+    device_store.on('state', function(state){
+      if(typeof(WhWidgetSendButton)=='undefined')
+        initFbChatButton(options);
+
+      var is_connected = state.current.status == "connected";
+      setTimeout(function(){
+        if(options.hide_on_offline){
+          if(is_connected)
+            showIcon();
+          else
+            hideIcon();
+        }
+      }, 1000)
       
     })
     initFbChatButton(options, function(){
-      if(options.hide_on_offline && device_store.get().status != 'connected'){
-        document.getElementById("wh-widget-send-button").style.display = 'none';
-      }
+      setTimeout(function(){
+        if(options.hide_on_offline && device_store.get().status != 'connected'){
+          hideIcon();
+        }
+      }, 1000)
     })
-  }, 1000);
+  }, 3000);
 })
