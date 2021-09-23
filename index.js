4c39147e3e6c4d908a21217a1e8c20be926114cb
Arnel Lenteria <arnel.lenteria@gmail.com>
2021-09-23 20:55:13 +0800

    80d486a5c773acd7aae69f563509b97b2afb2eec

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-'use strict'
-var { app } = require('plugin-core')
-var router = require('./router')
-var models = require('./models')
-
-module.exports = {
-  async init () {
-    await models.init()
-    app.use(router)
-  }
-}
+"use strict";var{app}=require("plugin-core");var router=require("./router");var models=require("./models");module.exports={async init(){await models.init();app.use(router)}};
